87313c7d48f805cd401690d4cb8ce4e883c00d98
Kevin Patel <kevin36524@gmail.com>
2013-08-13 17:54:02 +0530

    Initial Commit

---
+++ SimpleTodos/TodoAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TodoAppDelegate.h
+//  SimpleTodos
+//
+//  Created by Kevin Patel on 8/13/13.
+//  Copyright (c) 2013 Kevin Patel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TodoAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SimpleTodos/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SimpleTodos
+//
+//  Created by Kevin Patel on 8/13/13.
+//  Copyright (c) 2013 Kevin Patel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TodoAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TodoAppDelegate class]));
+    }
+}
